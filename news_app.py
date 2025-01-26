1fe93f55d47c1a54e6e04c4f4825676851c9d548
Aashif-k <ashif.aachi.k@gmail.com>
2025-01-26 23:45:23 +0530

    news_app.py

--- news_app.py
+++ news_app.py
@@ -33,6 +33,7 @@ page_pg_img="""
 [data-testid="stAppViewContainer"]{
 position: Relative;
 background-image: url("https://img.freepik.com/free-photo/newspaper-background-concept_23-2149501639.jpg?t=st=1737541856~exp=1737545456~hmac=1920514c5558ac13db21c9fd536ac3dd59bfecd149946cd1c62233789e01b2ee&w=996");
+opacity: 0.4;
 width: 100%;
 height: 100%;
 background-size: auto auto;
