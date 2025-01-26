bd488bbc3a9240087274ab3610f4cb12ac47e181
Aashif-k <ashif.aachi.k@gmail.com>
2025-01-26 22:00:29 +0530

    news_app.py

--- news_app.py
+++ news_app.py
@@ -47,7 +47,7 @@ def main():
         vectorizer = pickle.load(vectorizer_file)
 
     # User input
-    user_input = st.text_area("blue:[Enter news text to analyze:]")
+    user_input = st.text_area(":blue[Enter news text to analyze:]")
     l,ll,lm,m,rm,lr,r = st.columns(7)
     if m.button("Analyze"):
         progress_text = "Operation in progress. Please wait."
